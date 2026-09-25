e331d40bd622c3f4c8cc74c862355c2e89900c56
aquasilva111-ui <aquasilva111@gmail.com>
2026-09-25 14:32:29 -0600

    Add files via upload

---
+++ middleware.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getSessionCookie } from "better-auth/cookies";
+
+export async function middleware(request: NextRequest) {
+  const sessionCookie = getSessionCookie(request); // Optionally pass config as the second argument if cookie name or prefix is customized.
+  if (!sessionCookie) {
+    return NextResponse.redirect(new URL("/sign-in", request.url));
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    "/studio(.*)",
+    "/subscriptions",
+    "/feed/subscribed",
+    "/playlists(.*)",
+  ], // Specify the routes the middleware applies to
+};
